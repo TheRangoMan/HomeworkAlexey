689d5543f61073b6beb2455bfe6696c3b23d52c9
Alexey Motak <alex.motak@gmail.com>
2023-03-09 21:52:15 +0200

    homework

--- homework3.py
+++ homework3.py
@@ -1,11 +1,10 @@
 import json
-
 try:
-    with open(r'C:\Users\Alexey\Desktop\Python 2.0\HomeworkAlexey\homework2.py') as file_object:
+    with open(r"C:\Users\Alexey\Desktop\Python 2.0\HomeworkAlexey\homework2.py", encoding="utf8") as file_object:
         
         for line in file_object:
             print(line)      
-        json.dumps(object, ensure_ascii=False)    
+            
 except FileNotFoundError:
     print("No such file exists!")
 except IOError:
