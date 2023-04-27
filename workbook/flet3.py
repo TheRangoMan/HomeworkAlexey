0d2c814389fc98b5616121165242e415c8324e0c
Alexey Motak <alex.motak@gmail.com>
2023-04-27 13:06:16 +0300

    update

--- workbook/flet3.py
+++ workbook/flet3.py
@@ -1,12 +1,13 @@
-import flet as ft 
+import flet as ft
 from flet import Page, Text, TextField, Column, Row, IconButton
+import numpy
 import pandas
 from flet.plotly_chart import PlotlyChart
 import plotly_express as px
-from solver import solve
-import numpy
 
-def main(page:Page):
+from solver import solve
+
+def main(page: Page):
     
     def draw(e):
         a = int(startx.value)
@@ -16,13 +17,14 @@ def main(page:Page):
         funcs = func.value
         
         try:
-            values = list(map(lambda t: solve(funcs.replace('@x', f"{(t)}")), data))
-            
+            values = list(map(lambda t: solve(funcs.replace('@x', f"{t}")), data))
+            df = pandas.DataFrame(dict(x=data,y=values))
+            fig = px.line(df, x="x", y="y", title=f"User function = {funcs}")
             column = Column([
                 rowf,
                 rowrange,
                 PlotlyChart(fig, expand=False)
-            ],scroll=ft.ScrollMode.ADAPTIVE)
+            ], scroll=ft.ScrollMode.ADAPTIVE)
             page.clean()
             page.add(column)
             page.update()
@@ -30,26 +32,27 @@ def main(page:Page):
             column = Column([
                 rowf,
                 rowrange,
-                Text(str(ex),size=40, color = ft.colors.RED)
+                Text(str(ex), size=40, color=ft.colors.RED)
             ], scroll=ft.ScrollMode.ADAPTIVE)
             page.clean()
             page.add(column)
             page.update()
+        
     
-    text = Text('Enter your function with variable as @x', size=30)
-    func = TextField(value= " ", text_size=30)
+    text = Text('Enter you function with variable as @x', size=30)
+    func = TextField(value="", text_size=30)
     buttonstart = IconButton(icon=ft.icons.CALCULATE_ROUNDED, icon_size=60, on_click=draw)
     
     textStartLabel = Text('Enter start value', size=30)
-    startx = TextField(value=" ",text_size=30)
+    startx = TextField(value="", text_size=30)
     
-    textEndLabel = Text('Enter end value', size=30)
-    endx = TextField(value=" ", text_size=30)
+    textEndtLabel = Text('Enter end value', size=30)
+    endx = TextField(value="", text_size=30)
     
-    rowf = Row([text,func, buttonstart])
-    rowrange = Row([textStartLabel, startx, textEndLabel, endx])
-     
-    column = Column([rowf,rowrange])
+    rowf = Row([text, func, buttonstart])
+    rowrange = Row([textStartLabel, startx,textEndtLabel, endx])
+    
+    column = Column([rowf, rowrange])
     page.add(column)
-
+    
 ft.app(target=main)
